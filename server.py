b6c0006193a2681c327dc7d9932e43011701ce82
jsipkovsky <34807717+jsipkovsky@users.noreply.github.com>
2019-09-17 16:19:22 +0200

    Update server.py

--- server.py
+++ server.py
@@ -15,13 +15,25 @@ def index():
   crypto_name = data['conversation']['memory']['crypto']['raw']
 
   # FETCH BTC/USD/EUR PRICES
-  r = requests.get("https://min-api.cryptocompare.com/data/price?fsym="+crypto_name+"&tsyms=BTC,USD,EUR")
+  #r = requests.get("https://min-api.cryptocompare.com/data/price?fsym="+crypto_name+"&tsyms=BTC,USD,EUR")
+  
+  headers = {'content-type': 'application/json'}
+  # headers = { 'Authorization' : 'Token ' + token }
+  aa = str(133)
+  r = requests.get("https://my307428.crm.ondemand.com/sap/c4c/odata/v1/contract/ContractCollection?$filter=ID%20eq%20%27" + aa + "%27&$format=json", auth=('mpospisil', 'Oblacek123'), json={"key": "value"})
+
+  #ss = r.json()
+  #json_data = json.loads(r.text)
+  aa = r.text.find('ProcessingTypeCode')
+  bbb = r.text[int(aa + 21): int(aa + 26)]
+  aaa = r.json() # ["BuyerPartyMainContactPartyID"]
 
   return jsonify(
     status=200,
     replies=[{
       'type': 'text',
-      'content': 'The price of %s is %f BTC and %f USD' % (crypto_name, r.json()['BTC'], r.json()['USD'])
+      'content': 'The price + str(bbb)
+      #'content': 'The price of %s is %f BTC and %f USD' % (crypto_name, r.json()['BTC'], r.json()['USD'])
     }]
   )
 
