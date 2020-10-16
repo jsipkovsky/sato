a8b99c2f7bf79fa6b05b9a97b90b6cb9bb2f08f1
jsipkovsky <34807717+jsipkovsky@users.noreply.github.com>
2020-10-16 10:21:23 +0200

    Update server.py

--- server.py
+++ server.py
@@ -9,28 +9,10 @@ port = int(os.environ["PORT"])
 
 @app.route('/', methods=['POST'])
 def index():
-  data = json.loads(request.get_data())
-
-  # FETCH THE CRYPTO NAME
-  contact_id = data['conversation']['memory']['contract']['raw']
-
-  # FETCH BTC/USD/EUR PRICES
-  #r = requests.get("https://min-api.cryptocompare.com/data/price?fsym="+crypto_name+"&tsyms=BTC,USD,EUR")
+ 
   
-  headers = {'content-type': 'application/json'}
-  # headers = { 'Authorization' : 'Token ' + token }
-  aa = str(133)
-  r = requests.get("https://my307428.crm.ondemand.com/sap/c4c/odata/v1/contract/ContractCollection?$filter=ID%20eq%20%27" + contact_id + "%27&$format=json", auth=('mpospisil', 'Sojka123'), json={"key": "value"})
 
-  #ss = r.json()
-  #json_data = json.loads(r.text)
-  aa = r.text.find('ProcessingTypeCode')
-  bbb = r.text[int(aa + 21): int(aa + 26)]
-  aaa = r.json() # ["BuyerPartyMainContactPartyID"]
-  
-  cont = 'Contract ' + str(contact_id) + ' does not exist in the System, can you try again?'
-  if len(bbb) > 2:
-    cont = 'Your Order ' + str(contact_id) + ' will be delivered on Monday, 27th of March.'
+  cont = 'Your Order ' + str(contact_id) + ' will be delivered on Monday, 27th of March.'
 
   return jsonify(
     status=200,
